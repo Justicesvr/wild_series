fcf1424366c16aafb4fe6a76f7500d3805568817
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-05-25 18:29:12 +0200

    faker ok

--- src/DataFixtures/EpisodeFixtures.php
+++ src/DataFixtures/EpisodeFixtures.php
@@ -6,10 +6,46 @@ use App\Entity\Episode;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
 
 class EpisodeFixtures extends Fixture implements DependentFixtureInterface
 {
-    const EPISODES = [
+
+    public function load(ObjectManager $manager): void
+    {
+        //Puis ici nous demandons à la Factory de nous fournir un Faker
+        $faker = Factory::create();
+
+        /**
+         * L'objet $faker que tu récupère est l'outil qui va te permettre 
+         * de te générer toutes les données que tu souhaites
+         */
+
+        for ($i = 0; $i < 50; $i++) {
+            $episode = new Episode();
+            //Ce Faker va nous permettre d'alimenter l'instance de Season que l'on souhaite ajouter en base
+            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setTitle($faker->country());
+            $episode->setSynopsis($faker->paragraphs(3, true));
+
+            $episode->setSeason($this->getReference('season_' . $faker->numberBetween(0, 5)));
+
+            $manager->persist($episode);
+            $this->addReference('episode_' . $i, $episode);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            SeasonFixtures::class,
+        ];
+    }
+
+    /*const EPISODES = [
         [
             'title' => 'Days Gone Bye',
             'number' => '01',
@@ -80,5 +116,5 @@ class EpisodeFixtures extends Fixture implements DependentFixtureInterface
         return [
             SeasonFixtures::class,
         ];
-    }
+    }*/
 }
